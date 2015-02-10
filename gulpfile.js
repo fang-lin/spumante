3c31114d75e90fee1ae1993a38197e6dea67e83a
IsaacFang <isaac.fang@grapecity.com>
2015-02-10 14:11:13 +0800

    gulp build finished

--- gulpfile.js
+++ gulpfile.js
@@ -12,75 +12,185 @@ var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
     less = require('gulp-less'),
     minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    amdOptimize = require('amd-optimize'),
     rename = require('gulp-rename'),
-    copy = require('gulp-copy'),
+    copy2 = require('gulp-copy2'),
     sh = require('shelljs'),
     path = require('path'),
+    Q = require('q'),
     _ = require('underscore'),
     fs = require('fs');
 
+// endregion lint
 
-gulp.task('lint', function (done) {
-    gulp.src('./client/app/**/*.js')
+gulp.task('lint', function () {
+    return gulp.src([
+        'client/root/app/**/*.js',
+        'client/blog/app/**/*.js'
+    ])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
-        .on('end', done);
+        .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('less', function () {
-    gulp.src('./client/less/*.less')
+// endregion lint
+// region less
+
+gulp.task('less:root', ['bower'], function (done) {
+    return gulp.src('client/root/less/*.less')
         .pipe(less())
-        .pipe(gulp.dest('./client/css'))
-        .on('end', done);
+        .on('error', function (err) {
+            util.log(util.colors.red(err));
+        })
+        .pipe(gulp.dest('client/root/css/'));
 });
 
-gulp.task('develop', function () {
+gulp.task('less:blog', ['bower'], function (done) {
+    return gulp.src('client/blog/less/*.less')
+        .pipe(less())
+        .on('error', function (err) {
+            util.log(util.colors.red(err));
+        })
+        .pipe(gulp.dest('client/root/css/'));
+});
+
+// endregion less
+// region nodemon
+
+gulp.task('nodemon', function () {
     nodemon({
         script: 'app.js',
-        ext: 'html js',
-        env: {'NODE_ENV': 'development'},
-        nodeArgs: ['--debug'],
-        ignore: ['ignored.js']
+        ext: 'js json',
+        ignore: ['node_modules', 'client', 'dest'],
+        env: {'NODE_ENV': 'development'}
     }).on('restart', function () {
         util.log(util.colors.cyan('nodemon restarted'));
-    }).on('end', done);
+    });
 });
 
-gulp.task('watch', function (done) {
-    gulp.watch('./client/less/*.less', ['less'])
-        .on('end', done);
+// endregion nodemon
+// region watch
+
+gulp.task('watch', function () {
+    gulp.watch('./client/**/*.less', ['less']);
+    gulp.watch('./githook.hb', ['git-hook']);
 });
 
-gulp.task('bower', function (done) {
-    bower().on('end', done);
+// endregion watch
+// region bower
+
+gulp.task('bower', ['clean'], function (done) {
+    return bower();
 });
 
-gulp.task('minify-css', function (done) {
-    gulp.src('./client/css/main.css')
+// endregion bower
+// region minify-css
+
+gulp.task('minify-css:root', ['less:root'], function () {
+    return gulp.src('./client/root/css/main.css')
         .pipe(minifyCss({
             keepSpecialComments: 0
         }))
-        .pipe(gulp.dest('./dist/css/'))
-        .on('end', done);
+        .pipe(gulp.dest('./dist/root/css/'));
 });
 
+gulp.task('minify-css:blog', ['less:blog'], function () {
+    return gulp.src('./client/blog/css/main.css')
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest('./dist/root/css/'));
+});
+
+gulp.task('minify-css', ['minify-css:root', 'minify-css:blog']);
+
+// endregion minify-css
+// region git-hook
+
 gulp.task('git-hook', function (done) {
-    gulp.src('./git-hooks/*')
+    gulp.src('./githook.hb')
+        .pipe(rename('pre-commit'))
         .pipe(gulp.dest('./.git/hooks/'))
         .on('end', done);
 });
 
-gulp.task('clean', function (done) {
-    gulp.src('./dist/*')
-        .pipe(clean())
-        .on('end', done);
+// endregion git-hook
+// region clean
+
+gulp.task('clean', function () {
+    return gulp.src('./dist/*')
+        .pipe(clean());
 });
 
-gulp.task('copy', function (done) {
+// endregion clean
+// region compress
 
+gulp.task('compress:requirejs', ['bower'], function () {
+    gulp.src('client/lib/requirejs/require.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/lib/requirejs/'));
 });
 
-//gulp.task('build', ['bower', 'clean', 'less', 'minify-css', 'copy'], function (done) {
-gulp.task('build', function (done) {
-
+gulp.task('compress:root', ['bower'], function () {
+    return gulp.src('client/root/*').
+        pipe(amdOptimize('init', {
+            baseUrl: './client/root/',
+            configFile: './client/root/build.js'
+        }))
+        .pipe(concat('init.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/root'));
 });
+
+gulp.task('compress:blog', ['bower'], function () {
+    return gulp.src('client/blog/*').
+        pipe(amdOptimize('init', {
+            baseUrl: './client/blog/',
+            configFile: './client/blog/config.js'
+        }))
+        .pipe(concat('init.js'))
+        .pipe(gulp.dest('dist/root'));
+});
+
+gulp.task('compress', ['compress:requirejs', 'compress:root'/*, 'compress:blog'*/]);
+
+// endregion compress
+// region copy
+
+gulp.task('copy:lib', ['bower'], function () {
+    return copy2([
+        {src: 'client/lib/html5shiv/dist/html5shiv.min.js', dest: 'dist/lib/html5shiv/dist/'},
+        {src: 'client/lib/respond/dest/respond.min.js', dest: 'dist/lib/respond/dest/'}
+    ]);
+});
+
+gulp.task('copy:root', function () {
+    return copy2([
+        {src: 'client/root/index.html', dest: 'dist/root/'},
+        {src: 'client/root/css/all.css', dest: 'dist/root/css'},
+        {src: 'client/root/app/views/*', dest: 'dist/root/app/views/'},
+        {src: 'client/root/app/partials/*', dest: 'dist/root/app/partials/'},
+    ]);
+});
+
+gulp.task('copy:blog', function () {
+    return copy2([
+        {src: 'client/blog/index.html', dest: 'dist/blog/'},
+        {src: 'client/blog/css/all.css', dest: 'dist/blog/css'},
+        {src: 'client/blog/css/all.css', dest: 'dist/blog/css'},
+        {src: 'client/blog/app/views/*', dest: 'dist/blog/app/views/'},
+        {src: 'client/blog/app/partials/*', dest: 'dist/blog/app/partials/'}
+    ]);
+});
+
+gulp.task('copy', ['copy:lib', 'copy:root'/*, 'copy:blog'*/]);
+
+// endregion copy
+// region integration
+
+gulp.task('develop', ['nodemon', 'watch', 'git-hook']);
+
+gulp.task('build', ['copy', 'minify-css', 'compress']);
+
+// endregion integration
