ff3bc8414a1da9e295d60057fb3d3b286af22503
IsaacFang <isaac.fang@grapecity.com>
2015-02-12 17:31:03 +0800

    delete ejs tags space

--- client/root/init-build.js
+++ client/root/init-build.js
@@ -6,14 +6,14 @@
 requirejs.config({
     paths: {
         // Libs
-        'jquery': '/lib/jquery.min.js<&= tail("lib/jquery.min.js") &>',
-        'underscore': '/lib/underscore-min.js<&= tail("lib/underscore.min.js") &>',
-        'crypto': '/lib/crypto.min.js<&= tail("lib/crypto.min.js") &>',
-        'angular': '/lib/angular.min.js<&= tail("lib/angular.min.js") &>',
-        'angular-resource': '/lib/angular-resource.min.js<&= tail("lib/angular-resource.min.js") &>',
-        'angular-route': '/lib/angular-route.min.js<&= tail("lib/angular-route.min.js") &>',
-        'angular-animate': '/lib/angular-animate.min.js<&= tail("lib/angular-animate.min.js") &>',
-        'angular-translate': '/lib/angular-translate.min.js<&= tail("lib/angular-translate.min.js") &>',
+        'jquery': '/lib/jquery.min.js<&=tail("lib/jquery.min.js")&>',
+        'underscore': '/lib/underscore-min.js<&=tail("lib/underscore.min.js")&>',
+        'crypto': '/lib/crypto.min.js<&=tail("lib/crypto.min.js")&>',
+        'angular': '/lib/angular.min.js<&=tail("lib/angular.min.js")&>',
+        'angular-resource': '/lib/angular-resource.min.js<&=tail("lib/angular-resource.min.js")&>',
+        'angular-route': '/lib/angular-route.min.js<&=tail("lib/angular-route.min.js")&>',
+        'angular-animate': '/lib/angular-animate.min.js<&=tail("lib/angular-animate.min.js")&>',
+        'angular-translate': '/lib/angular-translate.min.js<&=tail("lib/angular-translate.min.js")&>',
         // Apps
         'config': 'app/config',
         'app': 'app/app',
